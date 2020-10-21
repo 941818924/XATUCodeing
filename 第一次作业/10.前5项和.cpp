ba7b6883e97ab59f475a99b2d56b11a381af7e16
MasterLong <941818924@qq.com>
2020-10-21 09:30:16 +0800

    10/21  9:30

--- 第一次作业/10.前5项和.cpp
+++ 第一次作业/10.前5项和.cpp
@@ -2,3 +2,25 @@
 求Sn=a+aa+aaa+aaaa+aaaaa的前5项之和，其中a是一个数字，
 例如：2+22+222+2222+22222
 */
+#include <iostream>
+
+using namespace std;
+
+int printSum(int a)
+{
+    int sum = 0, tmp = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        tmp = tmp * 10 + a;
+        sum += tmp;
+    }
+    return sum;
+}
+
+int main()
+{
+    int i;
+    cout << "please enter your number :" << endl;
+    cin >> i;
+    cout << "sum is : " << printSum(i) << endl;
+}

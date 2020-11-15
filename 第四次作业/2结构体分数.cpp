743f7a24cfe2456805087fab3d3fd411c753b1ea
MasterLong <941818924@qq.com>
2020-11-15 10:10:26 +0800

    考虑约分，化简

--- 第四次作业/2结构体分数.cpp
+++ 第四次作业/2结构体分数.cpp
@@ -11,20 +11,98 @@ struct Fenshu
     int fenmu;
 };
 
+int findNumber(int a, int b) //用最大公约数进行约分
+{
+    if (a < b)
+    {
+        int tmp = b;
+        b = a;
+        a = tmp;
+    }
+    int c = b;
+    while (a % b)
+    {
+        c = a % b;
+        a = b;
+        b = c;
+    }
+    return c;
+}
+
 void add(Fenshu a, Fenshu b)
 {
-    Fenshu result;
-    result.fenzi = a.fenzi * b.fenmu + b.fenzi * a.fenmu;
-    result.fenmu = a.fenmu * b.fenmu;
-    cout << "相加等于: " << result.fenzi << "/" << result.fenmu << endl;
+    int fz = a.fenzi * b.fenmu + b.fenzi * a.fenmu;
+    int fm = a.fenmu * b.fenmu;
+    pair<int, int> p(fz / findNumber(fz, fm), fm / findNumber(fz, fm));
+    if (p.second == 1) //考虑化简
+    {
+        cout << "相加等于: " << p.first << endl;
+    }
+    else if (p.second == -1)
+    {
+        cout << "相加等于: " << 0 - p.first << endl;
+    }
+    else
+    {
+        cout << "相加等于: " << p.first << "/" << p.second << endl;
+    }
+}
+
+void sub(Fenshu a, Fenshu b)
+{
+    int fz = a.fenzi * b.fenmu - b.fenzi * a.fenmu;
+    int fm = a.fenmu * b.fenmu;
+    pair<int, int> p(fz / findNumber(fz, fm), fm / findNumber(fz, fm));
+    if (p.second == 1)
+    {
+        cout << "相减等于: " << p.first << endl;
+    }
+    else if (p.second == -1)
+    {
+        cout << "相减等于: " << 0 - p.first << endl;
+    }
+    else
+    {
+        cout << "相减等于: " << p.first << "/" << p.second << endl;
+    }
 }
 
 void mul(Fenshu a, Fenshu b)
 {
-    Fenshu result;
-    result.fenzi = a.fenzi * b.fenzi;
-    result.fenmu = a.fenmu * b.fenmu;
-    cout << "相乘等于: " << result.fenzi << "/" << result.fenmu << endl;
+    int fz = a.fenzi * b.fenzi;
+    int fm = a.fenmu * b.fenmu;
+    pair<int, int> p(fz / findNumber(fz, fm), fm / findNumber(fz, fm));
+    if (p.second == 1)
+    {
+        cout << "相乘等于: " << p.first << endl;
+    }
+    else if (p.second == -1)
+    {
+        cout << "相乘等于: " << 0 - p.first << endl;
+    }
+    else
+    {
+        cout << "相乘等于: " << p.first << "/" << p.second << endl;
+    }
+}
+
+void div(Fenshu a, Fenshu b)
+{
+    int fz = a.fenzi * b.fenmu;
+    int fm = a.fenmu * b.fenzi;
+    pair<int, int> p(fz / findNumber(fz, fm), fm / findNumber(fz, fm));
+    if (p.second == 1)
+    {
+        cout << "相除等于: " << p.first << endl;
+    }
+    else if (p.second == -1)
+    {
+        cout << "相除等于: " << 0 - p.first << endl;
+    }
+    else
+    {
+        cout << "相除等于: " << p.first << "/" << p.second << endl;
+    }
 }
 
 int main()
@@ -33,7 +111,10 @@ int main()
     Fenshu b = {1, 6};
     cout << "分数a为： " << a.fenzi << "/" << a.fenmu << endl;
     cout << "分数b为： " << b.fenzi << "/" << b.fenmu << endl;
+    cout << endl;
     add(a, b);
+    sub(a, b);
     mul(a, b);
+    div(a, b);
     return 0;
 }
